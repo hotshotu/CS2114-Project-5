6bc295443632d6b05b815cdacff1090be7e5c555
hotshotu <44956829+hotshotu@users.noreply.github.com>
2018-11-12 22:44:19 -0500

    Update Glyph.java

--- Glyph.java
+++ Glyph.java
@@ -39,14 +39,99 @@ public class Glyph {
         r4Shape = new Shape(0, 0, 0, 0, Color.GREEN);
         glyphBlackBar = new Shape(0, 0, 0, 0);
     }
-    
-    public void update(Representation rep, Song song, int glyphNum) {
-        switch(rep) {
+
+
+    public void update(Representation rep, Song song, int numResponse) {
+        int rep1HWidth;
+        int rep1LWidth;
+        int rep2HWidth;
+        int rep2LWidth;
+        int rep3HWidth;
+        int rep3LWidth;
+        int rep4HWidth;
+        int rep4LWidth;
+        switch (rep) {
             default:
                 throw new IllegalArgumentException(
                     "Invalid Representation pass to Glyph");
             case HOBBY:
-                
+                rep1HWidth = song.getRepresentation(Representation.HOBBY)[0];
+                rep1LWidth = song.getRepresentation(Representation.HOBBY)[1];
+                rep2HWidth = song.getRepresentation(Representation.HOBBY)[2];
+                rep2LWidth = song.getRepresentation(Representation.HOBBY)[3];
+                rep3HWidth = song.getRepresentation(Representation.HOBBY)[4];
+                rep3LWidth = song.getRepresentation(Representation.HOBBY)[5];
+                rep4HWidth = song.getRepresentation(Representation.HOBBY)[6];
+                rep4LWidth = song.getRepresentation(Representation.HOBBY)[7];
+            case MAJOR:
+                rep1HWidth = song.getRepresentation(Representation.MAJOR)[0];
+                rep1LWidth = song.getRepresentation(Representation.MAJOR)[1];
+                rep2HWidth = song.getRepresentation(Representation.MAJOR)[2];
+                rep2LWidth = song.getRepresentation(Representation.MAJOR)[3];
+                rep3HWidth = song.getRepresentation(Representation.MAJOR)[4];
+                rep3LWidth = song.getRepresentation(Representation.MAJOR)[5];
+                rep4HWidth = song.getRepresentation(Representation.MAJOR)[6];
+                rep4LWidth = song.getRepresentation(Representation.MAJOR)[7];
+            case REGION:
+                rep1HWidth = song.getRepresentation(Representation.REGION)[0];
+                rep1LWidth = song.getRepresentation(Representation.REGION)[1];
+                rep2HWidth = song.getRepresentation(Representation.REGION)[2];
+                rep2LWidth = song.getRepresentation(Representation.REGION)[3];
+                rep3HWidth = song.getRepresentation(Representation.REGION)[4];
+                rep3LWidth = song.getRepresentation(Representation.REGION)[5];
+                rep4HWidth = song.getRepresentation(Representation.REGION)[6];
+                rep4LWidth = song.getRepresentation(Representation.REGION)[7];
         }
+        l1Shape = new Shape(rep1HWidth / numResponse, 15);
+        r1Shape = new Shape(0, 0, rep1LWidth / numResponse * 65, 15);
+        l2Shape = new Shape(rep2HWidth / numResponse, 15);
+        r2Shape = new Shape(0, 0, rep2LWidth / numResponse * 65, 15);
+        l3Shape = new Shape(rep3HWidth / numResponse, 15);
+        r3Shape = new Shape(0, 0, rep3LWidth / numResponse * 65, 15);
+        l4Shape = new Shape(rep4HWidth / numResponse, 15);
+        r4Shape = new Shape(0, 0, rep4LWidth / numResponse * 65, 15);
+        glyphBlackBar = new Shape(0, 0, 15, 60);
+    }
+
+
+// Need to account for variable bar lengths
+    public void displayGlyph(Window win, int x, int y) {
+
+        titleShape.setX((x - titleShape.getWidth()) / 2);
+        titleShape.setY(y);
+        artistShape.setX((y - artistShape.getWidth()) / 2);
+        artistShape.setY(y + titleShape.getHeight());
+
+        l1Shape.setX((x - l1Shape.getWidth()) / 2);
+        l1Shape.setY(y + titleShape.getHeight() + artistShape.getHeight());
+
+        r1Shape.setX(x + l1Shape.getWidth() + glyphBlackBar.getWidth());
+        r1Shape.setY(y + titleShape.getHeight() + artistShape.getHeight());
+
+        l2Shape.setX(x);
+        l2Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + l1Shape.getHeight());
+
+        r2Shape.setX(x + l2Shape.getWidth() + glyphBlackBar.getWidth());
+        r2Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + l1Shape.getHeight());
+
+        l3Shape.setX(x);
+        l3Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + l1Shape.getHeight() + l2Shape.getHeight());
+
+        r3Shape.setX(x + l3Shape.getWidth() + glyphBlackBar.getWidth());
+        r3Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + r1Shape.getHeight() + r2Shape.getHeight());
+
+        l4Shape.setX(x);
+        l4Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + l1Shape.getHeight() + l2Shape.getHeight() + l3Shape.getHeight());
+
+        r4Shape.setX(x + l4Shape.getWidth() + glyphBlackBar.getWidth());
+        r4Shape.setY(y + titleShape.getHeight() + artistShape.getHeight()
+            + r1Shape.getHeight() + r2Shape.getHeight() + r3Shape.getHeight());
+        
+        glyphBlackBar.setX(x + l1Shape.getWidth());
     }
 }
